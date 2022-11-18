a3d150cfbcd946db9d2cf36bf27a24c2620839f9
Zakaria Missou <106028299+HeaDLuCK@users.noreply.github.com>
2022-11-18 01:20:22 +0100

    Update MeteoData.py

--- MeteoData.py
+++ MeteoData.py
@@ -11,6 +11,7 @@ import re
 class MeteoApi:
 
     def __init__(self):
+        self.conn = MongoClient()
         self.api = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=donnees-synop-essentielles-omm&q=&facet=date&facet=nom&facet=temps_present&facet=libgeo&facet=nom_epci&facet=nom_dept&facet=nom_reg"
         self.param = {}
 
@@ -24,21 +25,7 @@ class MeteoApi:
         except FileExistsError:
             print("Folders already exists")
 
-
-    def connecet_db(self):
-        try:
-            self.conn = MongoClient()
-            print('Connected')
-            return self.conn
-        except:
-            print('Connect Failed')
-
-
-
-
-
-
-
+            
     def dateAndFormatValid(self,date):
         self.dateformate = "%Y-%m-%d"
 
@@ -68,8 +55,7 @@ class MeteoApi:
             return self.data
     
     def getDataforDb(self, date=None):
-        y = self.connecet_db()
-        self.col = y.MeteoDb.MeteoData
+        self.col = self.conn.MeteoDb.MeteoData
         ver = self.col.find()
         test = list(ver)
         if len(test) == 0:
@@ -104,23 +90,16 @@ class MeteoApi:
         print('Data entered successfully')
 
     def check_and_update(self):
-        y = self.connecet_db()
-        self.db=y.MeteoDb
-        dbnames=y.list_database_names()
+        self.db=self.conn.MeteoDb
+        dbnames=self.conn.list_database_names()
         if self.db.name not in dbnames:
             print('start')
             self.getDataforDb()
             self.check_and_update()
         else:
             self.col = self.db.MeteoData
-            firstTest=self.col.find().sort("date",-1).limit(1)
-            lastDate=None
-            for i in firstTest:
-                lastDate=i['date']
-            lastDate=lastDate.split("T")
-            lastDate=lastDate[0] 
+            lastDate=self.col.find().sort("date",-1).limit(1)[0]['date'].split("T")[0]
             lastDate=self.dateAndFormatValid(lastDate)
-
             currdate=datetime.date.today()
             minus=currdate-lastDate
             ver=int(minus.days)
@@ -131,26 +110,18 @@ class MeteoApi:
         print('All good ')
     
     def find_in_db2(self, nom, date):
-        
-            y = self.connecet_db()
             self.createFolders()
-            self.db=y.MeteoDb
-            self.col = self.db.MeteoData
-            currdate=datetime.date.today()
-            date=self.dateAndFormatValid(date)
+            self.col = self.conn.MeteoDb.MeteoData
             nom=nom.upper()
 
-            if date.year <= 2022 and date.year>=currdate.year:
-                date=str(date)
-                x = self.col.find({"nom": nom, "date": {'$regex': date}}).sort("date",1)
-                test = self.col.count_documents({"nom": nom, "date": {'$regex': date}})
-            
-                if test == 0:
-                    return "Error"
-                    
-                else:
-                    return x
-
+            x = self.col.find({"nom": nom, "date": {'$regex': date}}).sort("date",1)
+            test = self.col.count_documents({"nom": nom, "date": {'$regex': date}})
+        
+            if test == 0:
+                return "Error"
+                
+            else:
+                return x
 
 
     def graphs(self,nom,date):
@@ -158,52 +129,51 @@ class MeteoApi:
         path='static/data/graphs/'
         filename=nom+' '+date
         dirtest=os.listdir(path)
-        try:
-            for file in dirtest:
-                x=re.search(filename,file)
-                if x :
-                    raise StopIteration;
-            
+        for file in dirtest:
+            x=re.search(filename,file)
+            if x :
+                print('already exists')       
+                break;
+        else:
             for i in self.te:
                 self.graph(nom,date,i)  
-        except StopIteration:
-            print('already exists')       
+     
                 
     def graph(self, nom, date,ch='tc'):
         self.x=[]
         self.y=[]
         self.find=self.find_in_db2(nom, date)
-        self.Graph_dict={'pmer':["pression au miveau mer (Pa)",'Pa'],
-            'tc':['temperateur (C)','C°'],
-                'ff':['vitesse de vente moyen 10 mn (m/s)','m/s'],
-                    'u':['humidite(%)','%'],
-                    'pres':['pression station (Pa)','Pa']}
+        self.Graph_dict={'pmer':["pression au miveau mer ",'Pa'],
+            'tc':['temperateur','C°'],
+                'ff':['vitesse de vente moyen 10 mn','m/s'],
+                    'u':['humidite','%'],
+                    'pres':['pression station ','Pa']}
 
-        try:
-            for key,value in self.Graph_dict.items():
-                if key == ch:
-                    for i in self.find:
-                        if i[ch] != '':
-                            self.x.append(i['date'][11:16])
-                            self.y.append(i[ch])
-                            plt.figure()
-                            plt.switch_backend('agg')
-                            plt.bar(self.x, self.y, color = 'g', label = value[1])
-                            if ch=='pmer' or ch=='pres':
-                                plt.yticks(np.arange(90000, 108000, 4000),rotation = 60)
-                                plt.ylim(90000,108000)
-                            plt.xlabel('Time')
-                            plt.ylabel(value[0])
-                            plt.title(value[0]+'by time')
-                            path='static/data/graphs/'+nom+' '+date+' '+ch+'.png'
-                            plt.legend()
-                            plt.savefig(path)
-                            plt.close()
-                        else:
-                            raise StopIteration
-        except StopIteration:
-            print ("Data already exist in a File")
-            
+        for key,value in self.Graph_dict.items():
+            if key == ch:
+                for i in self.find:
+                    if i[ch] != '':
+                        self.x.append(i['date'][11:16])
+                        self.y.append(i[ch])
+                    else:
+                        print ("No more data")
+                        break
+
+                    plt.figure()
+                    plt.switch_backend('agg')
+                    plt.bar(self.x, self.y, color = 'g', label = value[1])
+                    if ch=='pmer' or ch=='pres':
+                        plt.yticks(np.arange(90000, 108000, 4000),rotation = 60)
+                        plt.ylim(90000,108000)
+                    plt.xlabel('Time')
+                    plt.ylabel(value[0])
+                    plt.title(value[0]+'by time')
+                    path='static/data/graphs/'+nom+' '+date+' '+ch+'.png'
+                    plt.legend()
+                    plt.savefig(path)
+                    plt.close()
+
+        #!Wordcloud    
     def wordcld(self,nom='orly',date='2022-11-8'):
         filename='WordCloud'
         path='static/img/'
